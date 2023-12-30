f3c1e13e7bbd02f1ffe60f894e832e1f10a92d9f
Daniel Yu <73201654+ArKaneVGC@users.noreply.github.com>
2023-12-29 19:48:07 -0500

    added 2nd pokemon

--- app.js
+++ app.js
@@ -68,9 +68,7 @@ $(".evs").bind("keyup change input", function () {
 const registerServiceWorker = async () => {
     if ('serviceWorker' in navigator) {
         try {
-            console.log("in");
             const registration = await navigator.serviceWorker.register("/serviceworker.js");
-            console.log("out");
             if (registration.installing) {
                 console.log("Service worker installing");
             } else if (registration.waiting) {
@@ -78,7 +76,6 @@ const registerServiceWorker = async () => {
             } else if (registration.active) {
                 console.log("Service worker active");
             } 
-            console.log(registration);
         }
         catch (error) {
             console.error(`Registration failed with ${error}`);
